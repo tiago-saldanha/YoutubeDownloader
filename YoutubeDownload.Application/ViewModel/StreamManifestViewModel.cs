39a8103da52400e76ae5553fa9eac7680035b4a4
Tiago <Tiago@XEON-2667V4>
2026-02-21 13:09:04 -0300

    refactor/remove-blazor-app

--- YoutubeDownload.Application/ViewModel/StreamManifestViewModel.cs
+++ YoutubeDownload.Application/ViewModel/StreamManifestViewModel.cs
@@ -8,9 +8,9 @@ namespace YoutubeDownload.Application.ViewModel
         public string ContainerName { get; private set; }
         public double Size { get; private set; }
         public bool IsAudioOnly { get; private set; }
-        public string? AudioCodec { get; private set; }
-        public string? Resolution { get; private set; }
-        public string? VideoCodec { get; private set; }
+        public string AudioCodec { get; private set; }
+        public string Resolution { get; private set; }
+        public string VideoCodec { get; private set; }
 
         public static StreamManifestViewModel Create(IStreamInfo stream, string url) => new(stream, url);
 
