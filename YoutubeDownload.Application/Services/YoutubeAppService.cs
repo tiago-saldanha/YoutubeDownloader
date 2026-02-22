47b6237e58e0b70fa05e9aa7bbf8878a0aa2aa6d
Tiago <Tiago@XEON-2667V4>
2026-02-22 19:40:12 -0300

    feature/add-youtube-app-service

---
+++ YoutubeDownload.Application/Services/YoutubeAppService.cs
@@ -0,0 +1,48 @@
+﻿using YoutubeExplode;
+using YoutubeDownload.Application.Interfaces;
+using YoutubeDownload.Application.Commands;
+using YoutubeDownload.Application.ViewModel;
+using Microsoft.Extensions.Logging;
+using YoutubeDownload.Application.Exceptions;
+using YoutubeDownload.Infrastructure.Interfaces;
+
+namespace YoutubeDownload.Application.Services
+{
+    public class YoutubeAppService(YoutubeClient client, IYoutubeService youtubeService, IFfmpegService ffmpegService, ILogger<YoutubeAppService> logger) 
+        : IYoutubeAppService
+    {
+        public async Task<StreamManifestViewModel> DownloadManifestAsync(string url)
+        {
+            try
+            {
+                var manifest = await youtubeService.DownloadManifestAsync(url);
+                return manifest;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error while downloading manifest for video [{Url}].", url);
+                throw new VideoManifestDownloadException(url, ex);
+            }
+        }
+
+        public async Task<DownloadStreamViewModel> DownloadAsync(DownloadCommand command)
+        {
+            try
+            {
+                logger.LogInformation("Starting video download [{Title}] (ID: {VideoId}).", command.Title, command.VideoId);
+
+                var manifest = await client.Videos.Streams.GetManifestAsync(command.VideoId);
+
+                return command.IsAudioOnly
+                    ? await youtubeService.DownloadAudioAsync(manifest, command)
+                    : await youtubeService.DownloadVideoAsync(manifest, command);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error while downloading video [{Title}] (ID: {VideoId}).", command.Title, command.VideoId);
+
+                throw new VideoDownloadException(command.VideoId, command.Title, ex);
+            }
+        }
+    }
+}
