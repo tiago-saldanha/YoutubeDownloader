22214c8afec072cd2d71f2bb88804f5e6df41244
Tiago <Tiago@XEON-2667V4>
2026-02-25 21:04:50 -0300

    feature/integrate with application layer

---
+++ YoutubeDownload.Blazor/Extensions/YoutubeDownloadBlazorExtensions.cs
@@ -0,0 +1,30 @@
+﻿using YoutubeExplode;
+using YoutubeDownload.Application.Interfaces;
+using YoutubeDownload.Application.Services;
+using YoutubeDownload.Infrastructure.Configuration;
+using YoutubeDownload.Infrastructure.Interfaces;
+using YoutubeDownload.Infrastructure.Services;
+
+namespace YoutubeDownload.Blazor.Extensions
+{
+    public static class YoutubeDownloadBlazorExtensions
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services) 
+            => services.ConfigureYoutubeClient();
+
+        private static IServiceCollection ConfigureYoutubeClient(this IServiceCollection services)
+        {
+            services.AddSingleton<YoutubeClient>();
+            services.AddScoped<IYoutubeAppService, YoutubeAppService>();
+            services.AddScoped<IYoutubeService, YoutubeService>();
+            services.AddSingleton<IFfmpegService, FfmpegService>();
+            return services;
+        }
+
+        public static WebApplicationBuilder AddInfraStructure(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<FfmpegOptions>(builder.Configuration.GetSection("Ffmpeg"));
+            return builder;
+        }
+    }
+}
--- YoutubeDownload.Blazor/Program.cs
+++ YoutubeDownload.Blazor/Program.cs
@@ -1,9 +1,13 @@
 using YoutubeDownload.Blazor.Components;
+using YoutubeDownload.Blazor.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.ConfigureServices();
+builder.AddInfraStructure();
 
 var app = builder.Build();
 
