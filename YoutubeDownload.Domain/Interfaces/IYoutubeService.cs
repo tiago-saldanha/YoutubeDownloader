d27c4f7654093a6072c9695d74d5d6311daed8ec
Tiago <Tiago@XEON-2667V4>
2026-03-01 13:12:58 -0300

    feature: add domain layer

---
+++ YoutubeDownload.Domain/Interfaces/IYoutubeService.cs
@@ -0,0 +1,14 @@
+﻿using YoutubeDownload.Domain.Commands;
+using YoutubeDownload.Domain.ViewModel;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownload.Domain.Interfaces
+{
+    public interface IYoutubeService
+    {
+        Task<StreamManifestViewModel> DownloadManifestAsync(string url);
+        Task<DownloadStreamViewModel> DownloadVideoAsync(StreamManifest manifest, DownloadCommand command);
+        Task<DownloadStreamViewModel> DownloadAudioAsync(StreamManifest manifest, DownloadCommand command);
+        Task ConverterAsync(string file);
+    }
+}
---
+++ YoutubeDownload.Domain/ViewModel/DownloadStreamViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace YoutubeDownload.Domain.ViewModel
+{
+    public readonly record struct DownloadStreamViewModel
+    {
+        public byte[] FileBytes { get; init; }
+        public string FileName { get; init; }
+        public string ContentType { get; } = "application/octet-stream";
+
+        public DownloadStreamViewModel(byte[] bytes, string fileName)
+        {
+            FileBytes = bytes;
+            FileName = fileName;
+        }
+
+        public static DownloadStreamViewModel Create(string filePath, string fileName)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException();
+            
+            var bytes = File.ReadAllBytes(filePath);
+
+            return new DownloadStreamViewModel(bytes, fileName);
+        }
+    }
+}
